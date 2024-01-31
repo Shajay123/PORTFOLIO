ebcb6f3c2350666a20bbc10897ae053c506dcd33
Shajay-123 <153935689+Shajay-123@users.noreply.github.com>
2024-01-31 18:41:11 +0530

    dg

--- port/settings.py
+++ port/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@qo=atk8fjnvpn-+&!6dc*8ao3*(_u_!+vaq+)1q_fsq_l3o)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hosting-inky.vercel.app']
 
 
 # Application definition
